6758f3370feba71aa1efe92dae2c5ce11caaa7eb
dinh phan <dinh.phancong@hungthinhcorp.com.vn>
2020-10-27 14:55:46 +0700

    Init Project

---
+++ src/main/java/ht/com/profile/controller/ProfileController.java
@@ -0,0 +1,26 @@
+package ht.com.profile.controller;
+
+import ht.com.profile.model.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/profile")
+public class ProfileController {
+
+    @GetMapping(value = "/{id}")
+    public Profile findOne(@PathVariable Long id) {
+        return new Profile("Dinh Phan", "0123456778");
+    }
+
+    @PostMapping(value = "/")
+    public Profile createUser(@RequestBody Profile profile) {
+
+        return new Profile("Dinh Phan", "0123456778");
+    }
+
+}
---
+++ src/main/java/ht/com/profile/model/Profile.java
@@ -0,0 +1,15 @@
+package ht.com.profile.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    private String name;
+    private String phoneNumber;
+}
