8a016ae3a2268432fbbdb07e9938a2ac82a8cbf7
dinh phan <dinh.phancong@hungthinhcorp.com.vn>
2020-12-10 18:33:23 +0700

    Refactor mongodb config.

--- profile-mgmt/src/main/java/org/ht/profile/config/ProfileDataSourceConfig.java
+++ profile-mgmt/src/main/java/org/ht/profile/config/ProfileDataSourceConfig.java
@@ -1,32 +1,17 @@
 package org.ht.profile.config;
 
-import com.mongodb.client.MongoClients;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.domain.EntityScanner;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.annotation.Persistent;
-import org.springframework.data.mapping.model.FieldNamingStrategy;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -37,41 +22,17 @@ import java.util.Collections;
 public class ProfileDataSourceConfig {
 
     private final MongoDbProperties mongoDbProperties;
-    private final ApplicationContext applicationContext;
 
-    @Bean(name = "profileMongoTemplate")
     @Primary
+    @Bean(name = "profileMongoTemplate")
     public MongoTemplate profileMongoTemplate() throws Exception {
-        MongoDatabaseFactory factory = mongoDbFactory(this.mongoDbProperties.getMongoProperties());
-        MongoCustomConversions conversions = new MongoCustomConversions(Collections.emptyList());
-        MongoConverter converter = mappingMongoConverter(factory, conversions, mongoMappingContext(conversions));
-        return new MongoTemplate(factory, converter);
+        return new MongoTemplate(profileMongoFactory(this.mongoDbProperties.getMongoProperties()));
+
     }
 
-    public MongoDatabaseFactory mongoDbFactory(final MongoProperties mongoProperties) {
-        return new SimpleMongoClientDatabaseFactory(MongoClients.create(mongoProperties.getUri()), mongoProperties.getDatabase());
+    @Primary
+    @Bean
+    public MongoDatabaseFactory profileMongoFactory(final MongoProperties mongo) {
+        return new SimpleMongoClientDatabaseFactory(mongo.getUri());
     }
-
-    public MongoMappingContext mongoMappingContext(MongoCustomConversions conversions)
-            throws ClassNotFoundException {
-        MongoMappingContext context = new MongoMappingContext();
-        context.setInitialEntitySet(new EntityScanner(this.applicationContext)
-                .scan(Document.class, Persistent.class));
-        Class<?> strategyClass = this.mongoDbProperties.getMongoProperties().getFieldNamingStrategy();
-        if (strategyClass != null) {
-            context.setFieldNamingStrategy(
-                    (FieldNamingStrategy) BeanUtils.instantiateClass(strategyClass));
-        }
-        context.setSimpleTypeHolder(conversions.getSimpleTypeHolder());
-        return context;
-    }
-
-    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory databaseFactory, MongoCustomConversions customConversions, MongoMappingContext mappingContext) {
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(databaseFactory);
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mappingContext);
-        converter.setCustomConversions(customConversions);
-        converter.setCodecRegistryProvider(databaseFactory);
-        return converter;
-    }
-
 }
