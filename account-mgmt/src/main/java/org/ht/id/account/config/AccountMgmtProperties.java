17268c983de32bf9c83c845245b5cbc0dd707f15
Võ Minh Vô <vominhvo@gmail.com>
2020-12-17 17:15:56 +0700

    AB#340 activation datetime expire configurable

--- account-mgmt/src/main/java/org/ht/id/account/config/AccountMgmtProperties.java
+++ account-mgmt/src/main/java/org/ht/id/account/config/AccountMgmtProperties.java
@@ -1,9 +1,11 @@
 package org.ht.id.account.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import lombok.Getter;
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 @Component
 @Getter
@@ -21,4 +23,23 @@ public class AccountMgmtProperties {
     @Value("${account-mgmt.configuration.reset-password.default-link}")
     private String resetPasswordLink;
 
+    @Value("${account-mgmt.configuration.activation.expire-period-duration}")
+    private String activationExpiryPeriodDuration;
+
+    // Get the expiry time in seconds
+    private long getActivationExpiryDuration() {
+        try {
+            return Duration.parse(activationExpiryPeriodDuration).getSeconds();
+        } catch (DateTimeParseException ex) {
+            // By default, get from expire-period-days and convert number of days to total of seconds
+            return activationExpirePeriodDays * 24 * 3600;
+        }
+    }
+
+    // Get the expiry of activation datetime
+    public Date getActivationExpiryDate() {
+        Date currentDate = new Date();
+        // Convert the current datetime to miliseconds
+        return new Date(currentDate.getTime() + getActivationExpiryDuration() * 1000);
+    }
 }
